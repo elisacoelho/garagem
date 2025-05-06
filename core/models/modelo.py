0bd8cdde5e18df5061939b10e209ac6b8bd99e41
elisacoelho <elisacoeljo@gmail.com>
2025-05-06 15:49:21 -0300

    feat: criação api modelo

--- core/models/modelo.py
+++ core/models/modelo.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Modelo(models.Model):
     nome = models.CharField(max_length=80)
-    marca = models.CharField(max_length=80)
+    marca = models.CharField(max_length=80, blank=True)
     categoria = models.CharField(max_length=80, blank=True)
 
     def __str__(self):
